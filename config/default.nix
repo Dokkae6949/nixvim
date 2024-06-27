87f381cb6d58272cbc86784cbfa0fb21e1e776f1
Dokkae6949 <finnliry@gmail.com>
2024-06-27 10:46:58 +0200

    Add default.nix for plugins

--- config/default.nix
+++ config/default.nix
@@ -7,28 +7,11 @@
     ./options.nix
     ./keymaps.nix
     ./clipboard.nix
-
-    # plugins
-    ./plugins/bufferline.nix
-    ./plugins/lualine.nix
-    ./plugins/lsp.nix
+    
+    ./plugins
   ];
-
+  
   extraPackages = with pkgs; [
     ripgrep
   ];
-
-  plugins = {
-    # fuzzy finder thingy
-    telescope.enable = true;
-
-    # better file browser
-    oil.enable = true;
-
-    # better default syntax highlighting
-    treesitter.enable = true;
-
-    # code snippets
-    luasnip.enable = true;
-  };
 }

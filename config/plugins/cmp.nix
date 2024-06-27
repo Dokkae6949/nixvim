3def97930cbd5bdd55de2014cdb511982c24a086
Dokkae6949 <finnliry@gmail.com>
2024-06-27 17:07:36 +0200

    Update cmp key-mappings

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -14,10 +14,10 @@
       ];
 
       mapping = {
-        "<C-x>" = "cmp.mapping.complete()";
-        "<C-S-k>" = "cmp.mapping.scroll_docs(-4)";
-        "<C-c>" = "cmp.mapping.close()";
-        "<C-S-j>" = "cmp.mapping.scroll_docs(4)";
+        "<C-y>" = "cmp.mapping.complete()";
+        "<C-K>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-x>" = "cmp.mapping.close()";
+        "<C-J>" = "cmp.mapping.scroll_docs(4)";
         "<Tab>" = "cmp.mapping.confirm({ select = true })";
         "<C-k>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
         "<C-j>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";

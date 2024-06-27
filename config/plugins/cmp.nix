d6da572ff8d923f8ce228b4ce70a5a35ff475816
Dokkae6949 <finnliry@gmail.com>
2024-06-27 11:48:45 +0200

    Fix auto completion

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -4,5 +4,24 @@
   plugins.cmp = {
     enable = true;
     autoEnableSources = true;
+
+    settings = {
+      sources = [
+        {name = "nvim_lsp";}
+        {name = "buffer";}
+        {name = "path";}
+        {name = "luasnip";}
+      ];
+
+      mapping = {
+        "<C-x>" = "cmp.mapping.complete()";
+        "<C-S-k>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-c>" = "cmp.mapping.close()";
+        "<C-S-j>" = "cmp.mapping.scroll_docs(4)";
+        "<Tab>" = "cmp.mapping.confirm({ select = true })";
+        "<C-k>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+        "<C-j>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+      };
+    };
   };
 }

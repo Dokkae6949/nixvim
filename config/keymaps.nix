a58d448e1745999d3a4852db8d5654ab5f90b62e
Dokkae6949 <finnliry@gmail.com>
2024-08-21 14:38:12 +0200

    something something

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -11,12 +11,21 @@
     { key = "<13>";
       action = "<CR>"; }
 
+    { action = "<cmd>noh<CR>";
+      key = "<leader><escape>"; }
+
     ### PLUGINS ###
     # Telescope
     { action = "<cmd>Telescope live_grep<CR>";
       key = "<leader>g"; }
     { action = "<cmd>Telescope fd<CR>";
       key = "<leader>f"; }
+
+    # Lsp
+    { action = "<cmd>lua vim.lsp.buf.code_action()<CR>";
+      key = "<leader>a"; }
+    { action = "<cmd>lua vim.lsp.buf.rename()<CR>";
+      key = "<leader>r"; }
 
     # Oil
     { action = "<cmd>Oil<CR>";

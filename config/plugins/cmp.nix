7691a6bda1176019be694404bb62ef13e6967ad9
Dokkae6949 <finnliry@gmail.com>
2024-09-12 20:47:09 +0200

    Adjust gh copilot.lua settings

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -15,9 +15,9 @@
 
       mapping = {
         "<C-y>" = "cmp.mapping.complete()";
-        "<C-K>" = "cmp.mapping.scroll_docs(-4)";
-        "<C-x>" = "cmp.mapping.close()";
-        "<C-J>" = "cmp.mapping.scroll_docs(4)";
+        # "<C-K>" = "cmp.mapping.scroll_docs(-4)";
+        "<Esc>" = "cmp.mapping.close()";
+        # "<C-J>" = "cmp.mapping.scroll_docs(4)";
         "<Tab>" = "cmp.mapping.confirm({ select = true })";
         "<C-k>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
         "<C-j>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
--- config/plugins/copilot.nix
+++ config/plugins/copilot.nix
@@ -3,5 +3,23 @@
 {
   plugins.copilot-lua = {
     enable = true;
+
+    filetypes = {
+      markdown = true;
+    };
+
+    suggestion = {
+      enabled = true;
+      autoTrigger = true;
+
+      keymap = {
+        accept = "<C-l>";
+        acceptWord = "<C-h>";
+        dismiss = "<Esc>";
+
+        next = "<C-j>";
+        prev = "<C-k>";
+      };
+    };
   };
 }
